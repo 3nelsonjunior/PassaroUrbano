37d06e115eb8baa4e140dba1b961ac2a45572f49
Nelson Junior <3nelsonjunior@gmail.com>
2017-11-14 23:17:04 -0200

    AULA 93 - Criando um serviço para exibir ofertas

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,8 @@
+export class OfertasService {
+    //retorna um array de strings
+    public getOfertas(): Array<string>{
+        let ofertas = ['Oferta1','Oferta2','Oferta3']
+        return ofertas;
+    }
+
+}
